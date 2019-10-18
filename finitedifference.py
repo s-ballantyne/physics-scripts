220c430b75a756251af5dcecc222f6d9fcb2f568
Stuart Ballantyne <sballantyne@protonmail.com>
2019-10-18 22:56:11 +0100

    Finish finitedifference.py

--- finitedifference.py
+++ finitedifference.py
@@ -4,24 +4,24 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
+
 try:
 	has_scipy = True
-	from scipy.misc import derivative as scipy_ndiff
+	from scipy.misc import derivative as scipy_derivative
 except ImportError:
 	has_scipy = False
-	scipy_ndiff = lambda: 0
+	scipy_derivative = lambda: 0
 	pass
-
-
-def amax2(array_like, index_lookup):
-	"""Convenience function to find maximum in a numpy array and lookup from a second array at the same time"""
-	i = np.argmax(array_like)
-	return index_lookup[i], array_like[i]
 
 
 def forward_difference(f, x, h):
 	"""Forward difference method"""
 	return (f(x + h) - f(x)) / h
+
+
+def central_difference(f, x, h):
+	"""Central difference method"""
+	return (f(x + h) - f(x - h)) / (2 * h)
 
 
 def finite_difference(f, a, b, diff_func: callable, n: int = 100, endpoint: bool = True):
@@ -49,50 +49,56 @@ def exercise_one():
 	Exercise one
 	"""
 
+	"""Create a new figure for plotting results"""
+	figure = plot.figure()
+	axes = figure.add_subplot(111)
+
 	"""
 	Define f(x) and f'(x)
 	"""
 	f = lambda x: np.exp(-x * x)
-	fp = lambda x: -2 * x * np.exp(-x * x)
+	g = lambda x: -2 * x * f(x)
 
 	"""Evaluate forward difference"""
 	dy_dx, y, x = finite_difference(f, -2, 2, forward_difference)
 
-	err = fp(x) - dy_dx
+	"""Evaluate and find maximum error"""
+	err = g(x) - dy_dx
+	abs_err = np.abs(err)
+	x_max, abs_err_max = np.amax(abs_err), x[np.argmax(abs_err)]
+
+	"""Print maximum error, plot error on graph"""
+	print(f"x_max = {x_max:.12f} err = {abs_err_max:.15f}")
+	axes.plot(x, abs_err, label="error", color="r")
+
+	"""
+	https://docs.scipy.org/doc/numpy/reference/generated/numpy.logspace.html
+	Create logspace (similar linspace but log-scaled) for the step-size h between a and b
+	"""
+	a, b = -1, -12
+	h = np.logspace(a, b, num=np.abs(b - a + 1), base=10)
+
+	""""""
+	err = g(x_max) - forward_difference(f, x_max, h)
 	abs_err = np.abs(err)
 
-	x_max, abs_err_max = amax2(abs_err, x)
-	print(f"x_max = {x_max:.12f} err = {abs_err_max:.15f}")
-
-	h_space = np.logspace(-1, -12, num=12, base=10)
-	new_err = fp(x_max) - forward_difference(f, x_max, h_space)
-	abs_new_err = np.abs(new_err)
-
-	print("\n".join(f"step  = {h:.12f} err = {e:.15f}" for e, h in zip(abs_new_err, h_space)))
+	"""Print results to screen"""
+	print("\n".join(f"step  = {h:.12f} err = {e:.15f}" for h, e in zip(h, abs_err)))
 	print(	f"The error in the result decreases to a minimum of "
-			f"{np.amin(abs_new_err):.15f} (h = {h_space[np.argmin(abs_new_err)]}).\n"
+			f"{np.amin(abs_err):.15f} (h = {h[np.argmin(abs_err)]}).\n"
 			f"After that, it appears to increase as the step size gets smaller.\n"
 			f"Perhaps this is due to the accumulation of floating-point arithmetic errors,"
 			f" since real numbers cannot be exactly represented on a computer with finite"
 			f" precision, which may be magnified by the representation of the mathematical"
 			f" formula in this script.")
 
-	figure = plot.figure()
-	axes = figure.add_subplot(111)
-
-	axes.plot(x, y, label="$y = f(x)$", color="b")
-	axes.errorbar(x, dy_dx, err, label="$y = f^{\prime}(x)$", color="g")
-	axes.plot(x, err, label="error", color="r")
-	axes.scatter(x_max, abs_err_max, label="maximum error", color="black", marker="o")
+	# axes.plot(x, y, label="$y = f(x)$", color="b")
+	# axes.errorbar(x, dy_dx, err, label="$y = f^{\prime}(x)$", color="g")
+	# axes.scatter(x_max, abs_err_max, label="maximum error", color="black", marker="o")
 
 	axes.set_title(r"Exercise one: $f(x) = e^{-x^{2}}$")
 	axes.grid()
 	axes.legend()
-
-
-def central_difference(f, x, h):
-	"""Central difference method"""
-	return (f(x + h) - f(x - h)) / (2 * h)
 
 
 def exercise_two():
@@ -110,8 +116,8 @@ def exercise_two():
 	x_i = -0.5
 	h = 1e-4
 	if has_scipy:
-		"""Uses the central difference method."""
-		print(f"dy/dx at x = {x_i}, step = {h}, using SciPy's derivative: {scipy_ndiff(f, x_i, h):.4f}")
+		"""SciPy uses the same method."""
+		print(f"dy/dx at x = {x_i}, step = {h}, using SciPy's derivative: {scipy_derivative(f, x_i, h):.4f}")
 
 	central = central_difference(f, x_i, h)
 	print(f"dy/dx at x = {x_i}, step = {h}, using central difference: {central:.4f}")
@@ -138,21 +144,33 @@ def exercise_two():
 	axes.legend()
 
 
+def grad(f, coords, steps):
+	"""
+	:param f: function to calculate derivative of
+	:param coords: tuple of coordinates of interest (x, y, etc.)
+	:param steps: step sizes for each coordinate
+	:return: derivative approximation for each coordinate
+	"""
+	return [(f(*(coords[:i] + (coord + step,) + coords[i+1:])) - f(*(coords[:i] + (coord - step,) + coords[i+1:]))) / (2 * step) for i, (coord, step) in enumerate(zip(coords, steps))]
+
+
+def directional_derivative(f, coords, vector, steps):
+	return np.dot(grad(f, coords, steps), vector / np.linalg.norm(vector))
+
+
 def exercise_three():
 	"""
-	Exercise three:
-		f(x) = cos(pi * exp(-x))
-
+	Exercise three
 	"""
 
 	"""Define f(x, y)"""
 	f = lambda x, y: np.sin(x * np.arccos(y))
 
-	scheme_x = "f_x(x, y) = (f(x + h, y) - f(x - h, y)) / 2h"
-	scheme_y = "f_y(x, y) = (f(x, y + k) - f(x, y - k)) / 2k"
-	print(f"My scheme is:\n\t{scheme_x}\n\t{scheme_y}")
+	scheme_x = "(f(x + h, y) - f(x - h, y)) / 2h"
+	scheme_y = "(f(x, y + k) - f(x, y - k)) / 2k"
 
-
+	print(f"My scheme is: \n\t{scheme_x}\n\t{scheme_y}")
+	print(f"directional derivative: {directional_derivative(f, coords=(1., 0.5), vector=(2., -1.), steps=(0.001, 0.001))}")
 
 
 if __name__ == "__main__":
